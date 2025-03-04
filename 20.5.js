79f14a9dcae8e8a6d23fcafb85643ef739ca3317
Gaabuto <khanh040604@gmail.com>
2025-03-04 15:59:10 +0700

    20.5.js

--- 20.5.js
+++ 20.5.js
@@ -1,7 +1,7 @@
 let a = +prompt("Mời bạn nhập 1 số bất kỳ");
 let b = +prompt("Mời bạn nhập 1 số bất kỳ");
 let c = 1;
-if( isNaN(a) || isNaN(b)){
+if(b < 0 || isNaN(a) || isNaN(b)){
     console.log("du lieu nhap vao k hop le");
 }else{
     for ( let i = 1; i <= b; i++){
